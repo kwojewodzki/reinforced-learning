fda5eb51ffa8183e6f9e6ef6d01ed105c0b9eb20
kwojewodzki <kwojewodzki99@gmail.com>
2022-01-02 22:59:49 +0100

    Random moving car added

--- src/cars/random_car.py
+++ src/cars/random_car.py
@@ -1,6 +1,40 @@
-import numpy.random as rand
+import random as rand
 
 class Random_car:
+    visited_vert = []
 
-    def __init__(self) -> None:
-        pass
+    def throw_dice(self,max):
+        num = rand.randint(0,max)
+        return num
+
+    def __init__(self,adj,graph,start,finish):
+
+        self.graph = graph
+        self.adj = adj
+        self.start = start
+        self.finish = finish
+
+    def random_move(self,curr_vert):
+
+        dice_throw = self.throw_dice(len(self.adj[curr_vert])-1)
+        next_vert = self.adj[curr_vert][dice_throw]
+        self.visited_vert.append(next_vert)
+        return next_vert
+
+    def solve_maze(self):
+        self.visited_vert.append(self.start)
+        curr_vert = self.start
+        next_vert = 0
+        crashed = False
+        iterations = 0
+
+        while curr_vert != self.finish:
+            iterations += 1
+            next_vert = self.random_move(curr_vert)
+            curr_vert = next_vert
+            if self.graph[next_vert][1] < 0:
+                crashed = True
+                break
+                
+
+        return crashed
--- src/main.py
+++ src/main.py
@@ -5,7 +5,14 @@ import sys
 
 
 if __name__ == "__main__":
-    start = int(sys.argv[1]) - 1
-    finish = int(sys.argv[2]) - 1
+    start = int(sys.argv[1])
+    finish = int(sys.argv[2])
 
     maze = gen_maze.Maze(start,finish)
+    car = rc.Random_car(maze.adj,maze.graph,start,finish)
+    crashed = True
+    i = 0
+    while crashed:
+        crashed = car.solve_maze()
+        i+=1
+    print("Solved in ", i, "iterations")
