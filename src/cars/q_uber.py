9a5fb59062d2ffc7e7508af02882fdd0d35315a8
kwojewodzki <kwojewodzki99@gmail.com>
2022-01-03 18:51:10 +0100

    Startet q_uber class

--- src/cars/q_uber.py
+++ src/cars/q_uber.py
@@ -1,7 +1,16 @@
 import numpy.random as rand
 
+#actions: 
+# 0 - up
+# 1 - down
+# 2 - right
+# 3 - left
+
 class Q_uber:
     visited_vert = []
+    states = [i for i in range(64)]
+    P = [[[],[],[],[]] for i in range[64]]
+    epsilon = 0.1
 
     def __init__(self,adj,graph,start,finish):
 
@@ -10,5 +19,12 @@ class Q_uber:
         self.start = start
         self.finish = finish
 
+    def reset_position(self):
+        return self.start
+
     def solve_maze(self):
-        pass
+        curr_position = self.start
+
+        while curr_position != self.finish:
+            pass
+            
